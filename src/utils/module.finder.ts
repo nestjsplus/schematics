89c25cad3ee4610cb3695d47406e1edf50a65e53
John Biundo <johnfbiundo@gmail.com>
2019-10-02 14:02:24 -0700

    realign package, export utils

--- src/utils/module.finder.ts
+++ src/utils/module.finder.ts
@@ -13,7 +13,7 @@ export class ModuleFinder {
   public find(options: FindOptions): Path | null {
     const generatedDirectoryPath: Path = options.path;
     const generatedDirectory: DirEntry = this.tree.getDir(
-      generatedDirectoryPath,
+      generatedDirectoryPath
     );
     return this.findIn(generatedDirectory);
   }
@@ -23,7 +23,7 @@ export class ModuleFinder {
       return null;
     }
     const moduleFilename: PathFragment = directory.subfiles.find(filename =>
-      /\.module\.(t|j)s$/.test(filename),
+      /\.module\.(t|j)s$/.test(filename)
     );
     return moduleFilename !== undefined
       ? join(directory.path, moduleFilename.valueOf())
